111b6a4dba83ef199c2c04ff4755ff9091337e13
aid003 <teremochek859@gmail.com>
2024-12-14 20:14:27 +0300

    d3d3d3

--- controllers/ValidatePayment.service.js
+++ controllers/ValidatePayment.service.js
@@ -4,21 +4,23 @@ import { bot_tg, prisma } from "../index.js";
 export const validatePayment = expressAsyncHandler(async (req, res) => {
   const { order_id, amount, profit } = req.body;
 
-  if (!order_id || !amount || profit) {
-    throw new Error("Ошибка обработки успешного заказа");
-  }
+  console.log(req.body);
 
-  try {
-    const pay = await prisma.payment.update({
-      where: { order_id: order_id },
-      data: { status: true },
-      select: {
-        author: true,
-      },
-    });
-    await bot_tg.sendDocument(pay.author.tgId, "../public/материалы.txt");
-  } catch (error) {
-    console.log(error);
-    throw new Error(`${order_id} не обновлен. Ошибка пополнения.`);
-  }
+  // if (!order_id || !amount || profit) {
+  //   throw new Error("Ошибка обработки успешного заказа");
+  // }
+
+  // try {
+  //   const pay = await prisma.payment.update({
+  //     where: { order_id: order_id },
+  //     data: { status: true },
+  //     select: {
+  //       author: true,
+  //     },
+  //   });
+  //   await bot_tg.sendDocument(pay.author.tgId, "../public/материалы.txt");
+  // } catch (error) {
+  //   console.log(error);
+  //   throw new Error(`${order_id} не обновлен. Ошибка пополнения.`);
+  // }
 });
